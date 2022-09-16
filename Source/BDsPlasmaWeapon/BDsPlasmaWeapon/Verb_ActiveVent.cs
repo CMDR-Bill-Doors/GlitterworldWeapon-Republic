89953fa3eb52be37545b3a5d25c2509632b5a4af
CMDR-Bill-Doors <Billyanghk@qq.com>
2022-09-16 12:31:33 +0100

    Jump Function

---
+++ Source/BDsPlasmaWeapon/BDsPlasmaWeapon/Verb_ActiveVent.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace BDsPlasmaWeapon
+{
+    public class Verb_ActiveVent : Verb
+    {
+        public new CompReloadableFromFiller ReloadableCompSource => DirectOwner as CompReloadableFromFiller;
+
+        protected override bool TryCastShot()
+        {
+            Pop(ReloadableCompSource);
+            return true;
+        }
+
+        public override float HighlightFieldRadiusAroundTarget(out bool needLOSToCenter)
+        {
+            needLOSToCenter = false;
+            float radius = ReloadableCompSource.compActiveVentData.Props.radius;
+            int consumption = ReloadableCompSource.compActiveVentData.Props.maxConsumption;
+            if (ReloadableCompSource.remainingCharges < consumption)
+            {
+                radius = radius * ((float)ReloadableCompSource.remainingCharges / (float)consumption);
+            }
+            return radius;
+        }
+
+        public override void DrawHighlight(LocalTargetInfo target)
+        {
+            DrawHighlightFieldRadiusAroundTarget(caster);
+        }
+
+        public void Pop(CompReloadableFromFiller comp)
+        {
+            if (comp != null && comp.CanBeUsed && ReloadableCompSource.compActiveVentData != null)
+            {
+                float radius = ReloadableCompSource.compActiveVentData.Props.radius;
+                int consumption = ReloadableCompSource.compActiveVentData.Props.maxConsumption;
+                if (comp.remainingCharges < consumption)
+                {
+                    radius = radius * ((float)comp.remainingCharges / (float)consumption);
+                    consumption = comp.remainingCharges;
+                }
+                Pawn wearer = comp.Wearer;
+                GenExplosion.DoExplosion(wearer.Position, wearer.Map, radius, RimWorld.DamageDefOf.Extinguish, null, -1, -1f, null, null, null, null, RimWorld.ThingDefOf.Gas_Smoke, 1f);
+                comp.DrawGas(consumption);
+            }
+        }
+    }
+
+    public class CompActiveVentDataInterface : ThingComp
+    {
+        public CompProperties_ActiveVentDataInterface Props
+        {
+            get
+            {
+                return (CompProperties_ActiveVentDataInterface)props;
+            }
+        }
+    }
+
+
+    public class CompProperties_ActiveVentDataInterface : CompProperties
+    {
+        public string Icon = "UI/Commands/DesirePower";
+        public string Label = "shield on";
+        public string description = "";
+        public float radius = 5;
+        public int maxConsumption = 100;
+        public float heatPushPerUnit = -1;
+
+        public CompProperties_ActiveVentDataInterface()
+        {
+            compClass = typeof(CompActiveVentDataInterface);
+        }
+    }
+}
---
+++ Source/BDsPlasmaWeapon/BDsPlasmaWeapon/Verb_GasJump.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace BDsPlasmaWeapon
+{
+    public class Verb_GasJump : Verb_Jump
+    {
+        public new CompReloadableFromFiller ReloadableCompSource => DirectOwner as CompReloadableFromFiller;
+
+        public override bool MultiSelect => true;
+        protected override float EffectiveRange
+        {
+            get
+            {
+                float radius = ReloadableCompSource.compGasJumpData.Props.radius;
+                int consumption = ReloadableCompSource.compGasJumpData.Props.maxConsumption;
+                if (ReloadableCompSource.remainingCharges < consumption)
+                {
+                    radius = radius * ((float)ReloadableCompSource.remainingCharges / (float)consumption);
+                }
+                return radius;
+            }
+        }
+
+        protected override bool TryCastShot()
+        {
+            if (!ModLister.CheckRoyalty("Jumping"))
+            {
+                return false;
+            }
+            if (ReloadableCompSource == null || !ReloadableCompSource.CanBeUsed || ReloadableCompSource.compGasJumpData == null)
+            {
+                return false;
+            }
+            Pawn casterPawn = CasterPawn;
+            if (casterPawn == null || ReloadableCompSource == null || !ReloadableCompSource.CanBeUsed)
+            {
+                return false;
+            }
+            IntVec3 cell = currentTarget.Cell;
+            Map map = casterPawn.Map;
+            GenExplosion.DoExplosion(casterPawn.Position, casterPawn.Map, ReloadableCompSource.compGasJumpData.Props.BlastCloudRadius, RimWorld.DamageDefOf.Extinguish, null, -1, -1f, null, null, null, null, RimWorld.ThingDefOf.Gas_Smoke, 1f);
+            ReloadableCompSource.DrawGas(ReloadableCompSource.compGasJumpData.Props.maxConsumption);
+            PawnFlyer pawnFlyer = PawnFlyer.MakeFlyer(RimWorld.ThingDefOf.PawnJumper, casterPawn, cell);
+            if (pawnFlyer != null)
+            {
+                GenSpawn.Spawn(pawnFlyer, cell, map);
+                return true;
+            }
+            return false;
+        }
+        public override void DrawHighlight(LocalTargetInfo target)
+        {
+            if (target.IsValid && ValidJumpTarget(caster.Map, target.Cell))
+            {
+                GenDraw.DrawTargetHighlightWithLayer(target.CenterVector3, AltitudeLayer.MetaOverlays);
+            }
+            GenDraw.DrawRadiusRing(caster.Position, EffectiveRange, Color.white, (IntVec3 c) => GenSight.LineOfSight(caster.Position, c, caster.Map) && ValidJumpTarget(caster.Map, c));
+        }
+    }
+
+    public class CompGasJumpDataInterface : ThingComp
+    {
+        public CompProperties_GasJumpDataInterface Props
+        {
+            get
+            {
+                return (CompProperties_GasJumpDataInterface)props;
+            }
+        }
+    }
+
+
+    public class CompProperties_GasJumpDataInterface : CompProperties
+    {
+        public string Icon = "UI/Commands/DesirePower";
+        public string Label = "shield on";
+        public string description = "";
+        public float radius = 5;
+        public int maxConsumption = 100;
+        public float BlastCloudRadius = 2;
+
+        public CompProperties_GasJumpDataInterface()
+        {
+            compClass = typeof(CompGasJumpDataInterface);
+        }
+    }
+}
